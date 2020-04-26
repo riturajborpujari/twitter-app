d66f3ce43ea38d89ac248990c401e6061547ae69
Rituraj Borpujari <riturajborpujari@outlook.com>
2020-04-26 20:07:28 +0530

    performed cleanup

--- src/routes/v1/index.ts
+++ src/routes/v1/index.ts
@@ -1,28 +1,8 @@
 import express from 'express';
-import { SuccessResponse, SuccessMsgResponse} from '../../core/ApiResponse';
-import { InternalError, AuthError, ForbiddenError, BadRequestError } from '../../core/ApiError';
-import AsyncHandler from '../../helper/asyncHandler';
+import twitterRouter from './twitter';
 
 const router = express.Router();
 
-router.get('/success', (req, res, next) => {
-    return new SuccessMsgResponse().send(res);
-})
-
-router.get('/bad_request', AsyncHandler(async (req, res, next) => {
-    throw new BadRequestError()
-}))
-
-router.get('/error', (req, res, next) => {
-    try {
-        throw new ForbiddenError()
-    } catch (error) {
-        next(error)
-    }
-})
-
-router.get('/success_data', (req, res, next) => {
-    return new SuccessResponse({owner: 'Rituraj Borpujari', info: 'API server'}).send(res);
-})
+router.use('/twitter', twitterRouter);
 
 export default router;
