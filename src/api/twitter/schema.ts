1247437223f6924e4175e316572e8a5203bdecae
Rituraj Borpujari <riturajborpujari@outlook.com>
2020-04-28 22:31:59 +0530

    Added schema for filtered tweets, user, hashtags, ...

---
+++ src/api/twitter/schema.ts
@@ -0,0 +1,78 @@
+export interface ITweet {
+    created_at: string,
+    id_str: string,
+    text: string,
+    meta: {
+        favorite_count: number,
+        retweet_count: number,
+        is_retweet: boolean,
+        is_quote_status: boolean,
+        lang: string,
+        truncated: boolean
+    },
+    location_info: {
+        coordinates?: string,
+        place?: string,
+    },
+    user: IUser,
+    in_reply_to?: {
+        status_id_str?: string,
+        user_id_str?: string,
+        screen_name?: string
+    },
+    entities?: {
+        urls?: IUrl[],
+        hastags?: IHashtag[],
+        user_mentions?: IUserMention[]
+    },
+    original_status?: ITweet
+};
+
+export interface IUser {
+    id_str: string,
+    name: string,
+    screen_name: string,
+    description: string,
+    location: string,
+    url?: string,
+    profile: {
+        background_image_url: string,
+        image_url: string,
+        banner_url: string,
+        followers_count: number,
+        friends_count: number,
+        meta: {
+            created_at: string,
+            verified: boolean,
+            statuses_count: number
+        }
+    }
+};
+
+export interface IUrl {
+    url: string,
+    expanded_url: string,
+    indices: [
+        number,
+        number
+    ]
+};
+
+export interface IHashtag {
+    text: string,
+    indices: [
+        number,
+        number
+    ]
+};
+
+export interface IUserMention {
+    screen_name: string,
+    name: string,
+    
+    id_str: string,
+    indices: [
+        number,
+        number
+    ]
+}
