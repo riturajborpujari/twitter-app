90cb4f7dca98cd4861f69a7f6e4294c9bc72fb63
Rituraj Borpujari <riturajborpujari@outlook.com>
2020-04-28 22:32:27 +0530

    Added filter to twitter response

---
+++ src/api/twitter/filter/tweetFilter.ts
@@ -0,0 +1,147 @@
+import {ITweet, IUser, IUrl, IHashtag, IUserMention} from '../schema';
+
+const FilterTweets = (_tweets: any[]): ITweet[] => {
+    let tweets: ITweet[] = [];
+
+    _tweets.forEach(item => {
+        let tweet_item = FilterTweet(item);
+        tweets.push(tweet_item);
+    })
+
+    return tweets;
+}
+
+const FilterTweet = (tweet: any): ITweet => {
+    let {created_at, id_str, entities, user, text, full_text,
+        in_reply_to_status_id_str, in_reply_to_user_id_str,in_reply_to_screen_name, 
+        coordinates, place, favorite_count, retweet_count, lang, retweeted_status,
+        is_quote_status, truncated} = tweet
+    
+    let filteredTweet: ITweet = {
+        created_at,
+        id_str,
+        text: full_text? full_text : text,
+        meta: {
+            favorite_count,
+            retweet_count,
+            is_retweet: !retweeted_status? false: true,
+            is_quote_status,
+            lang,
+            truncated
+        },
+        location_info: {
+            coordinates,
+            place
+        },
+        user: FilterUser(user),
+        in_reply_to: {
+            status_id_str: in_reply_to_status_id_str,
+            user_id_str: in_reply_to_user_id_str,
+            screen_name: in_reply_to_screen_name
+        },
+        original_status: !retweeted_status? null : FilterTweet(retweeted_status)
+    }
+
+    if(entities){
+        filteredTweet.entities = FilterEntities(entities);
+    }
+
+    if(is_quote_status){
+        filteredTweet.original_status = FilterTweet(tweet.quoted_status);
+    }
+
+    return filteredTweet;
+}
+
+const FilterUser = (_user: any) => {
+    let {id_str, name, screen_name, description, location, url, created_at,
+        followers_count,friends_count,verified, statuses_count,
+        profile_background_image_url, profile_image_url, profile_banner_url} = _user;
+
+    let user: IUser = {
+        id_str,
+        name,
+        screen_name,
+        description,
+        location,
+        url,
+        profile: {
+            background_image_url: profile_background_image_url,
+            image_url: profile_image_url,
+            banner_url: profile_banner_url,
+            followers_count,
+            friends_count,
+            meta: {
+                created_at, 
+                verified,
+                statuses_count
+            }
+        }
+    };
+
+    return user;
+}
+
+const FilterEntities = (_entities: any) => {
+    let {hashtags: _hashtags, 
+        urls: _urls, 
+        user_mentions: _user_mentions} = _entities;
+
+    let hashtags = FilterHashtags(_hashtags);
+    let urls = FilterUrls(_urls);
+    let user_mentions = FilterUserMentions(_user_mentions);
+
+    return {hashtags, urls, user_mentions};;
+}
+
+const FilterUrls = (_urls: any[]) => {
+    let urls: IUrl[] = [];
+
+    _urls.forEach(item => {
+        let {url, expanded_url, indices} = item;
+        let url_item: IUrl = {
+            url,
+            expanded_url,
+            indices
+        };
+        urls.push(url_item)
+    })
+
+    return urls;
+}
+
+const FilterHashtags = (_hashtags: any[]) => {
+    let hashtags: IHashtag[] = [];
+
+    _hashtags.forEach(item => {
+        let {text, indices} = item;
+
+        let hashtag_item: IHashtag = {
+            text,
+            indices
+        };
+        hashtags.push(hashtag_item);
+    })
+
+    return hashtags;
+}
+
+const FilterUserMentions = (_userMentions: any[]) => {
+    let userMentions: IUserMention[] = [];
+
+    _userMentions.forEach(item => {
+        let {screen_name, name, id_str, indices} = item;
+
+        let userMentionItem: IUserMention = {
+            screen_name,
+            name,
+            id_str,
+            indices
+        };
+        userMentions.push(userMentionItem);
+    })
+
+    return userMentions;
+}
+
+export default FilterTweets;
--- src/api/twitter/index.ts
+++ src/api/twitter/index.ts
@@ -1,4 +1,11 @@
 import Twitter from 'twitter';
+import FilterTweets from './filter/tweetFilter';
+import { ITweet } from './schema';
+
+export interface ITweetsResponse{
+    tweets: ITweet[],
+    search_metadata: any
+}
 
 export default class TwitterClient {
     private client: Twitter
@@ -7,15 +14,23 @@ export default class TwitterClient {
         this.client = new Twitter(twitterOptions);
     }
 
-    async searchTweets(params: Twitter.RequestParams) : Promise<Twitter.ResponseData>{
-        return this.client.get('search/tweets', params);
+    async searchTweets(params: Twitter.RequestParams) : Promise<ITweetsResponse>{
+        let response = await this.client.get('search/tweets', params);
+
+        let tweets: ITweet[] = await FilterTweets(response.statuses);
+
+        return {tweets, search_metadata: response.search_metadata};
     }
 
     async postTweet(status: string) : Promise<Twitter.ResponseData>{
         return this.client.post('status/update', {status});
     }
 
-    async getExtendedTweets(ids: string): Promise<Twitter.ResponseData> {
-        return this.client.get(`statuses/lookup`, {id: ids, tweet_mode:'extended'});
+    async getExtendedTweets(ids: string): Promise<ITweetsResponse> {
+        let response = await this.client.get(`statuses/lookup`, {id: ids, tweet_mode:'extended'});
+
+        let tweets: ITweet[] = await FilterTweets(<any[]>response);
+
+        return {tweets, search_metadata: response.search_metadata};
     }
 };
