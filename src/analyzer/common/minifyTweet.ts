d5cd80585aa4d98e216b474170c65820c3fdb805
Rituraj Borpujari <riturajborpujari@outlook.com>
2020-05-12 20:37:13 +0530

    minifyTweet can optionally include user or ignore

--- src/analyzer/common/minifyTweet.ts
+++ src/analyzer/common/minifyTweet.ts
@@ -2,15 +2,14 @@ import * as TweetsSchema from '../../api/twitter/schema';
 import * as AnalysisSchema from '../schema';
 import MinifyUser from './minifyUser';
 
-export default function MinifyTweet(full_tweet: TweetsSchema.ITweet): AnalysisSchema.IMinifiedTweet {
+export default function MinifyTweet(full_tweet: TweetsSchema.ITweet, include_user:boolean = true): AnalysisSchema.IMinifiedTweet {
     let minified_tweet:AnalysisSchema.IMinifiedTweet =  {
         created_at: full_tweet.created_at,
         text: full_tweet.text,
         engagement: {
             favorite_count: full_tweet.meta.favorite_count,
             retweet_count: full_tweet.meta.retweet_count
-        },
-        user: MinifyUser(full_tweet.user)
+        }
     };
 
     if(full_tweet.entities){
@@ -29,6 +28,10 @@ export default function MinifyTweet(full_tweet: TweetsSchema.ITweet): AnalysisSc
         if(!hashtagsFound && !mentionsFound) {
             delete minified_tweet.entities;
         }
+    }
+
+    if(include_user){
+        minified_tweet.user = MinifyUser(full_tweet.user);
     }
 
     return minified_tweet;
